6174599503df40a0f42936e295ebaae2c7088bd0
Rafael Raniere de Oliveira <102255059+Pequeno-Samurai@users.noreply.github.com>
2022-11-13 18:45:48 -0300

    Update exercicio02.c

--- exercicio02.c
+++ exercicio02.c
@@ -3,19 +3,28 @@ calcular e mostrar:
 A menor altura do grupo;
 A maior altura do grupo.*/
 #include <stdio.h>
-int main (){
-	int n, contador;
-	float maior, menor, altura;
-	n = 15, contador = 0, maior = 99999, menor = 99999;
-	while (contador < n){
+
+int main ()
+{
+	int n = 15, contador = 0;
+	float maior=99999, menor=99999, altura;
+
+	while (contador < n)
+	{
 		contador++;
 		printf("Digite uma altura: ");
 		scanf("%f", &altura);
-		if(maior <altura){
-			maior = altura; }
-		if (menor > altura){
-			menor = altura; }
+		if(maior <altura)
+		{
+			maior = altura; 
+		}
+		if (menor > altura)
+		{
+			menor = altura; 
+		}
 	}
+	
 	printf("Maior altura: %.2f\n", maior);
 	printf("Menor altura: %.2f\n", menor);
+	
 	return 0; }
