280319a26c2dd00aeb4d891b6bc6511a73b150ea
Rafael Raniere de Oliveira <102255059+Pequeno-Samurai@users.noreply.github.com>
2022-11-13 18:51:28 -0300

    Update exercicio06.c

--- exercicio06.c
+++ exercicio06.c
@@ -2,15 +2,22 @@
 calcule a tabuada de N. Mostre a tabuada na forma: 0 N = 0; 1 N = 1N; 2 N = 2N;
 ; 10 N = 10N.*/
 #include <stdio.h>
-int main (){
+
+int main ()
+{
 	int cont=0, a=11, x, mult=0, result;
+	
 	printf("Digite um valor: ");
 	scanf("%d", &x);
-	while (cont < a){
+	
+	while (cont < a)
+	{
 		cont++;
-		if(x>=1 && x<+10){
+		if(x>=1 && x<+10)
+		{
 			result = x*mult;
 			printf("Tabuada de: %d\n", result);
 			mult++; }
 	}
-	return 0; }
+	return 0; 
+}
