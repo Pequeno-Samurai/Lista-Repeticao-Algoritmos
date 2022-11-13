10c7df1f0b55da73619856401d5f1d62998660e8
Rafael Raniere de Oliveira <102255059+Pequeno-Samurai@users.noreply.github.com>
2022-11-13 18:53:01 -0300

    Update exercicio07.c

--- exercicio07.c
+++ exercicio07.c
@@ -1,14 +1,22 @@
 /*Implemente um programa que leia um valor inicial A e imprima a sequência de
 valores do cálculo de A! e o seu resultado. Ex: 5! = 5 4 3 2 1 = 120*/
 #include <stdio.h>
-int main(){
+
+int main()
+{
 	int cont=1, a, result, b, n;
+	
 	printf("Digite o valor inicial: ");
 	scanf("%d", &a);
+	
 	b=a, n=a;
-	while (cont <n){
+	
+	while (cont <n)
+	{
 		cont++;
 		b=b-1;
-		a=a*b; }
+		a=a*b; 
+	}
 	printf("Sequencia de valores do calculo de A: %d", a);
-	return 0; }
+	return 0; 
+}
