c79419f05f672b6c84a94bd05a49dae10f198eba
Rafael Raniere de Oliveira <102255059+Pequeno-Samurai@users.noreply.github.com>
2022-11-13 18:50:35 -0300

    Update exercicio04.c

--- exercicio04.c
+++ exercicio04.c
@@ -2,18 +2,23 @@
 positivos. Calcule a quantidade de números pares e ímpares, a média de valores
 pares e a média geral dos números lidos. O número que encerra a leitura será zero.*/
 #include <stdio.h>
-int main (){
+
+int main ()
+{
 	int valor, par=0, somapar=0, impar=0, somaimpar=0;
 	float mediapar, mediageral;
 	
-	while (1){
+	while (1)
+	{
 		printf("Digite um valor positivo: ");
 		scanf("%d", &valor);
-		if (valor > 0){
+		if (valor > 0)
+		{
 			if (valor%2==0)
 				par++;
 				somapar+=par;
-				else if (valor%2=1){
+				else if (valor%2=1)
+				{
 					impar++;
 					somaimpar+=impar;
 				}
@@ -22,13 +27,5 @@ int main (){
 	
 	printf("Qantidade de valores pares: %d", pares);
 	printf("Qantidade de valores impares: %d", impares);
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 	return 0; }
